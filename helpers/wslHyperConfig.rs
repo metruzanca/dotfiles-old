5f0a715ade97f1cac0a6a1186b81f41de9bffb46
Metruzanca - gamingpc <metruzanca@gmail.com>
2020-06-03 08:25:21 +0200

    wslHyperConfig WIP

--- helpers/wslHyperConfig.rs
+++ helpers/wslHyperConfig.rs
@@ -1,6 +1,19 @@
-use std::fs;
+// use std::fs;
+
+extern crate dotenv;
+
+use std::env;
+use dotenv::dotenv;
 
 fn main() -> std::io::Result<()> {
-    fs::copy("../dot/.config/Hyper/.hyper.js", "/mnt/c/Users/Metruzanca/AppData/Roaming/Hyper/.hyper.js")?;  // Copy foo.txt to bar.txt
-    Ok(())
+    dotenv().ok();
+    // let args: Vec<String> = env::args().collect();
+
+    // let username = &args[1];
+    // fs::copy("../dot/.config/Hyper/.hyper.js", format!("/mnt/c/Users/{}/AppData/Roaming/Hyper/.hyper.js", username))?;  
+    // println!("/mnt/c/Users/{}/AppData/Roaming/Hyper/.hyper.js", username);
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+    // Ok(())
 }
